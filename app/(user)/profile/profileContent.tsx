e4c332fa2f8e4b06b63b58c93b5a63d502a1b006
Bramanda <putubramanda11@gmail.com>
2025-11-07 01:13:56 +0800

    fix ui profile

--- app/(user)/profile/profileContent.tsx
+++ app/(user)/profile/profileContent.tsx
@@ -63,72 +63,78 @@ export function ProfileContent({
               <AvatarImage src={userData?.name} alt={userData?.name} />
               <AvatarFallback>{userData?.name.charAt(0)}</AvatarFallback>
             </Avatar>
-            <div className="flex-1">
-              <div className="font-semibold text-lg text-gray-800">
-                {userData?.name}
+            <div className="w-full md:flex items-center space-y-4">
+              <div className="flex-1">
+                <div className="font-semibold text-lg text-gray-800">
+                  {userData?.name}
+                </div>
+                <div className="text-sm text-gray-500">
+                  {userData?.email || "Belum mengisi email"}
+                </div>
+                <div className="flex items-center gap-2 text-sm mt-2">
+                  <Phone size={16} className="text-primary" />
+                  <span className="text-gray-700">{userData?.phone}</span>
+                </div>
               </div>
-              <div className="text-sm text-gray-500">
-                {userData?.email || "Belum mengisi email"}
-              </div>
-              <div className="flex items-center gap-2 text-sm mt-2">
-                <Phone size={16} className="text-primary" />
-                <span className="text-gray-700">{userData?.phone}</span>
-              </div>
-            </div>
 
-            <Dialog open={open} onOpenChange={setOpen}>
-              <DialogTrigger asChild>
-                <Button
-                  size="sm"
-                  className="rounded-xl flex items-center gap-1 bg-primary text-white hover:bg-primary/90 cursor-pointer"
-                >
-                  <Edit3 size={14} /> Edit
-                </Button>
-              </DialogTrigger>
-              <DialogContent className="sm:max-w-[450px]">
-                <DialogHeader>
-                  <DialogTitle>Edit Profil</DialogTitle>
-                </DialogHeader>
-                <form className="space-y-4" action={action}>
-                  <InputField
-                    id="name"
-                    label="Nama"
-                    placeholder="Masukkan nama lengkap"
-                    defaultValue={state?.inputs?.name ?? userData?.name ?? ""}
-                    required
-                    autoComplete="name"
-                    error={state?.errors?.name}
-                  />
-                  <InputField
-                    id="email"
-                    label="Email"
-                    type="email"
-                    placeholder="Masukkan Email"
-                    defaultValue={state?.inputs?.email ?? userData?.email ?? ""}
-                    autoComplete="email"
-                    error={state?.errors?.email}
-                  />
-                  <InputField
-                    id="phone"
-                    label="Nomor HP"
-                    placeholder="Masukkan nomor HP"
-                    defaultValue={state?.inputs?.phone ?? userData?.phone ?? ""}
-                    required
-                    autoComplete="tel"
-                    error={state?.errors?.phone}
-                  />
-                  <DialogFooter>
-                    <Button
-                      disabled={isPending}
-                      type="submit"
-                      className="bg-primary text-white w-20"
-                    >
-                      {isPending ? <Spinner /> : "Simpan"}
-                    </Button>
-                  </DialogFooter>
-                </form>
-              </DialogContent>
-            </Dialog>
+              <Dialog open={open} onOpenChange={setOpen}>
+                <DialogTrigger asChild>
+                  <Button
+                    size="sm"
+                    className="rounded-xl flex items-center gap-1 bg-primary text-white hover:bg-primary/90 cursor-pointer"
+                  >
+                    <Edit3 size={14} /> Edit
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[450px]">
+                  <DialogHeader>
+                    <DialogTitle>Edit Profil</DialogTitle>
+                  </DialogHeader>
+                  <form className="space-y-4" action={action}>
+                    <InputField
+                      id="name"
+                      label="Nama"
+                      placeholder="Masukkan nama lengkap"
+                      defaultValue={state?.inputs?.name ?? userData?.name ?? ""}
+                      required
+                      autoComplete="name"
+                      error={state?.errors?.name}
+                    />
+                    <InputField
+                      id="email"
+                      label="Email"
+                      type="email"
+                      placeholder="Masukkan Email"
+                      defaultValue={
+                        state?.inputs?.email ?? userData?.email ?? ""
+                      }
+                      autoComplete="email"
+                      error={state?.errors?.email}
+                    />
+                    <InputField
+                      id="phone"
+                      label="Nomor HP"
+                      placeholder="Masukkan nomor HP"
+                      defaultValue={
+                        state?.inputs?.phone ?? userData?.phone ?? ""
+                      }
+                      required
+                      autoComplete="tel"
+                      error={state?.errors?.phone}
+                    />
+                    <DialogFooter>
+                      <Button
+                        disabled={isPending}
+                        type="submit"
+                        className="bg-primary text-white w-20"
+                      >
+                        {isPending ? <Spinner /> : "Simpan"}
+                      </Button>
+                    </DialogFooter>
+                  </form>
+                </DialogContent>
+              </Dialog>
+            </div>
           </div>
         </CardContent>
       </Card>
@@ -184,7 +190,7 @@ export function AddressContent({
       <CardHeader className="border-b pb-4">
         <CardTitle className="text-lg font-semibold">Alamat</CardTitle>
       </CardHeader>
-      <CardContent className="px-6 py-2 space-y-6">
+      <CardContent className="px-6 py-2 space-y-4">
         <div className="flex items-start gap-3">
           <MapPin size={18} className="text-primary mt-1" />
           <div className="capitalize">
